982431317a5ebe75a08052e56b590e95f908d7aa
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-08-09 20:35:12 +0530

    name corrected

--- lib/screens/features/auth/auth_screen.dart
+++ lib/screens/features/auth/auth_screen.dart
@@ -55,7 +55,7 @@ class _AuthScreen extends State<AuthScreen> {
                         ),
                         const SizedBox(height: 100),
                         Text(
-                          ' Welcome To Keepsafe ashdjasd ',
+                          ' Welcome To Keepsafe',
                           style: TextStyle(fontSize: 25, color: kwhite),
                           textAlign: TextAlign.start,
                         ),
