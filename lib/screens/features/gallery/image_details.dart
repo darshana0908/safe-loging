b9a9380b88c7e0f1f0f4567f53b1bfaa3e4ca9af
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-08-30 05:14:24 +0530

    new design added

--- lib/screens/features/gallery/image_details.dart
+++ lib/screens/features/gallery/image_details.dart
@@ -2,10 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../../constants/colors.dart';
-
-import 'package:share_plus/share_plus.dart';
 
 class ImageDetails extends StatefulWidget {
   final String path;
@@ -27,66 +26,59 @@ class _ImageDetailsState extends State<ImageDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: kdarkblue,
-          title: const Text('Image Deatils'),
+          backgroundColor: kblack,
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  child: Image.file(
-                    //load system image
-                    File(widget.path), //system image path
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 3 * 1.5,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                BottomAppBar(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        IconButton(
-                          onPressed: () async {
-                            final temp = await getTemporaryDirectory();
+        backgroundColor: kblack,
+        body: Column(
+          children: [
+            Center(
+              child: Image.file(
+                //load system image
+                File(widget.path), //system image path
 
-                            await Share.shareFiles([widget.path],
-                                text:
-                                    'check out my Apps https://Safeloging.com');
-                          },
-                          icon: Icon(
-                            Icons.share,
-                            color: kblue,
-                          ),
-                          iconSize: 50,
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.rotate_90_degrees_ccw_outlined,
-                            color: kblue,
-                          ),
-                          iconSize: 50,
-                        ),
-                        InkWell(
-                          onTap: () async {
-                            setState(() {
-                              delete(widget.path);
-                            });
-                          },
-                          child: Icon(
-                            Icons.delete_forever,
-                            color: kblue,
-                            size: 50,
-                          ),
-                        ),
-                      ]),
-                )
-              ],
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: kblack,
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            IconButton(
+              onPressed: () async {
+                final temp = await getTemporaryDirectory();
+
+                await Share.shareFiles([widget.path],
+                    text: 'check out my Apps https://Safeloging.com');
+              },
+              icon: Icon(
+                Icons.share,
+                color: kwhite,
+              ),
+              iconSize: 30,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.rotate_90_degrees_ccw_outlined,
+                color: kwhite,
+              ),
+              iconSize: 30,
+            ),
+            InkWell(
+              onTap: () async {
+                setState(() {
+                  delete(widget.path);
+                });
+              },
+              child: Icon(
+                Icons.delete_forever,
+                color: kwhite,
+                size: 30,
+              ),
+            ),
+          ]),
         ));
   }
 
