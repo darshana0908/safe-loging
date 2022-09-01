fdbbc34838fdfa7f6156b40290b5d3522567b528
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-09-01 22:31:50 +0530

    updated gellory

--- lib/screens/features/gallery/image_screen.dart
+++ lib/screens/features/gallery/image_screen.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:file_cryptor/file_cryptor.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hawk_fab_menu/hawk_fab_menu.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,17 +14,26 @@ import 'image_details.dart';
 
 class ImageScreen extends StatefulWidget {
   final String path;
-  const ImageScreen({Key? key, required this.path}) : super(key: key);
+  final String title;
+  const ImageScreen({Key? key, required this.path, required this.title})
+      : super(key: key);
 
   @override
   State<ImageScreen> createState() => _ImageScreenState();
 }
 
 class _ImageScreenState extends State<ImageScreen> {
+  String imageName = '';
+  String fileType = '';
   bool _isLoading = false;
+
+  int x = 1;
+
+  int size = 5;
   final ImagePicker _picker = ImagePicker();
   List<String> decryptedImages = [];
   int _countOfReload = 0;
+  bool imgload = false;
 
   @override
   void initState() {
@@ -39,40 +49,143 @@ class _ImageScreenState extends State<ImageScreen> {
         deleteDecryptedImages(decryptedImages);
         return true;
       },
-      child: Scaffold(
-        backgroundColor: kblack,
-        appBar: AppBar(
-          title: const Text('Images'),
-          backgroundColor: kdarkblue,
-        ),
-        body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: HawkFabMenu(
-              blur: 155.8,
-              backgroundColor: kliteblue,
-              openIcon: Icons.add,
-              closeIcon: Icons.close,
-              items: [
-                HawkFabMenuItem(
-                    label: 'Import photos',
-                    ontap: () async => importPhotos(),
-                    icon: const Icon(Icons.photo),
-                    color: const Color.fromRGBO(0, 0, 0, 0.38),
-                    labelColor: Colors.white,
-                    labelBackgroundColor: kliteblue),
-                HawkFabMenuItem(
-                  label: 'Take photo',
-                  ontap: () async => takePhoto(),
-                  icon: const Icon(Icons.camera_alt),
-                  color: Colors.black38,
-                  labelColor: Colors.white,
-                  labelBackgroundColor: kliteblue,
+      child: SafeArea(
+        child: Scaffold(
+            backgroundColor: kwhite,
+            body: Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  color: kwhite,
+                ),
+                Stack(
+                  children: [
+                    CustomScrollView(
+                      physics: const PageScrollPhysics(),
+                      slivers: [
+                        SliverAppBar(
+                          backgroundColor: kdarkblue,
+                          actions: [
+                            IconButton(
+                              icon: Icon(x == 1
+                                  ? Icons.view_list
+                                  : x == 2
+                                      ? Icons.view_module
+                                      : Icons.view_comfortable),
+                              padding: EdgeInsets.zero,
+                              onPressed: () {
+                                setState(() {
+                                  if (x == 1) {
+                                    setState(() {
+                                      x = 2;
+                                    });
+                                  } else if (x == 2) {
+                                    x = 3;
+                                  } else {
+                                    x = 1;
+                                  }
+                                });
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.more_vert),
+                              onPressed: () {},
+                            ),
+                          ],
+                          flexibleSpace: FlexibleSpaceBar(
+                              title: SizedBox(
+                                width: 250,
+                                height: 50,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(widget.title),
+                                    Text(
+                                      '${decryptedImages.length.toString()} Photos',
+                                      style:
+                                          TextStyle(color: kgray, fontSize: 11),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              background: imgload
+                                  ? Image.file(
+                                      File(
+                                        decryptedImages.last,
+                                      ),
+                                      fit: BoxFit.fill,
+                                    )
+                                  : Image.asset(
+                                      'assets/Capture5.JPG',
+                                      fit: BoxFit.fill,
+                                    )
+
+                              // Image.asset(
+                              //   '${widget.path}/$imageName',
+                              //   fit: BoxFit.fill,
+                              // ),
+                              ),
+                          floating: false,
+                          pinned: true,
+                          expandedHeight:
+                              MediaQuery.of(context).size.height * 0.305,
+                        ),
+                        SliverList(
+                            delegate: SliverChildListDelegate([
+                          SizedBox(
+                            height: 800,
+                            child: _isLoading
+                                ? const Center(
+                                    child: CupertinoActivityIndicator(
+                                      radius: 55,
+                                      color: Colors.red,
+                                    ),
+                                  )
+                                : loadPhotos(),
+                          ),
+                        ]))
+                      ],
+                    ),
+                    Positioned(
+                      child: SizedBox(
+                        height: double.infinity,
+                        width: double.infinity,
+                        child: HawkFabMenu(
+                          blur: 155.8,
+                          backgroundColor: kliteblue,
+                          openIcon: Icons.add,
+                          closeIcon: Icons.close,
+                          items: [
+                            HawkFabMenuItem(
+                                label: 'Import photos',
+                                ontap: () async => importPhotos(),
+                                icon: const Icon(Icons.photo),
+                                color: const Color.fromRGBO(0, 0, 0, 0.38),
+                                labelColor: Colors.white,
+                                labelBackgroundColor: kliteblue),
+                            HawkFabMenuItem(
+                              label: 'Take photo',
+                              ontap: () async => takePhoto(),
+                              icon: const Icon(Icons.camera_alt),
+                              color: Colors.black38,
+                              labelColor: Colors.white,
+                              labelBackgroundColor: kliteblue,
+                            ),
+                          ],
+                          body: const SizedBox(
+                              // child: _isLoading
+                              // ? const Center(
+                              //     child: CircularProgressIndicator())
+                              // : loadPhotos()),
+                              ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-              body: _isLoading
-                  ? const Center(child: CircularProgressIndicator())
-                  : loadPhotos()),
-        ),
+            )),
       ),
     );
   }
@@ -83,8 +196,12 @@ class _ImageScreenState extends State<ImageScreen> {
       return Container(
         padding: const EdgeInsets.only(bottom: 60.0),
         child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 6),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: x == 1
+                  ? 2
+                  : x == 2
+                      ? 4
+                      : 6),
           padding: const EdgeInsets.all(8.0),
           itemCount: decryptedImages.length,
           itemBuilder: (context, index) {
@@ -100,7 +217,7 @@ class _ImageScreenState extends State<ImageScreen> {
                   elevation: 8.0,
                   child: Hero(
                       tag: imgPath,
-                      child: Image.file(File(imgPath), fit: BoxFit.fill))),
+                      child: Image.file(File(imgPath), fit: BoxFit.fill,))),
             );
           },
         ),
@@ -119,9 +236,7 @@ class _ImageScreenState extends State<ImageScreen> {
   // import photos inside the folder
   importPhotos() async {
     log(widget.path);
-    String imageName = '';
-    String fileType = '';
-
+    print(imageName);
     final List<XFile>? imageList = await _picker.pickMultiImage();
 
     if (imageList != null) {
@@ -134,18 +249,19 @@ class _ImageScreenState extends State<ImageScreen> {
         fileToSave.copy('${widget.path}/$imageName');
 
         ImageService().encryptFiles(imageName, '$imageName.aes', widget.path);
-        setState(() => decryptedImages.add('${widget.path}/$imageName'));
+        setState(
+          () {
+            decryptedImages.add('${widget.path}/$imageName');
+          },
+        );
       }
     }
   }
 
   // take photos inside the folder
   takePhoto() async {
-    String imageName = '';
-    String fileType = '';
-
     final XFile? image = await _picker.pickImage(source: ImageSource.camera);
-
+    print(imageName);
     if (image != null) {
       fileType = path.extension(image.path);
       imageName =
@@ -185,7 +301,10 @@ class _ImageScreenState extends State<ImageScreen> {
         decryptedImages.add(decryptedFile.path);
       }
 
-      setState(() => _isLoading = false);
+      setState(() {
+        imgload = true;
+        _isLoading = false;
+      });
     } else {
       setState(() => _isLoading = false);
     }
@@ -217,5 +336,16 @@ class _ImageScreenState extends State<ImageScreen> {
     setState(() {
       _countOfReload = _countOfReload + 1;
     });
+  }
+}
+
+class imgload extends ChangeNotifier {
+  String decryptedImages = '';
+  String get getecryptedImages => decryptedImages;
+  img() {
+    Image.file(
+      File(decryptedImages.length.toString()),
+      fit: BoxFit.fill,
+    );
   }
 }
