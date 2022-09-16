697e4800a4297e1c89ec7a4cc9a6cd428c809a26
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-09-16 08:23:39 +0530

    svg image change

--- lib/screens/features/permission/permission_screen.dart
+++ lib/screens/features/permission/permission_screen.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:safe_encrypt/constants/colors.dart';
@@ -26,65 +25,73 @@ class _PermissionScreenState extends State<PermissionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kdarkblue,
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              const SizedBox(height: 150),
-              SvgPicture.asset(
-                'assets/pramisson_1.svg',
-                color: Colors.limeAccent,
-                width: 150,
-              ),
-              const SizedBox(height: 100),
-              Text(
-                'Keepsafe needs access to  your file storage',
-                style: TextStyle(fontSize: 25, color: kwhite),
-                textAlign: TextAlign.start,
-              ),
-              const SizedBox(height: 15),
-              Text(
-                'this allows keepsafe to access and  encrypt your photos or videos.',
-                style: TextStyle(fontSize: 17, color: kgray),
-                textAlign: TextAlign.start,
-              ),
-              const SizedBox(height: 25),
-              InkWell(
-                onTap: () async {
-                  if (await requestPermission(Permission.storage)) {
-                    await createFolder(realFolder);
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            SizedBox(height: MediaQuery.of(context).size.height / 6 * 1),
+            // SvgPicture.asset(
+            //   'assets/safepal-sfp-logo.svg',
+            //   color: Colors.black87,
+            //   width: 150,
+            // ),
+            SizedBox(
+                height: 230,
+                width: 280,
+                child: Image.asset(
+                  'assets/ic.JPG',
+                  fit: BoxFit.fill,
+                )),
+            SizedBox(height: MediaQuery.of(context).size.height / 6 * 1),
+            Text(
+              'Keepsafe needs access to  your file storage',
+              style: TextStyle(
+                  fontSize: 28, color: kwhite, fontWeight: FontWeight.w500),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 15),
+            Text(
+              'this allows keepsafe to access and  encrypt your photos or videos.',
+              style: TextStyle(
+                  fontSize: 19, color: kgray, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 25),
+            InkWell(
+              onTap: () async {
+                if (await requestPermission(Permission.storage)) {
+                  await createFolder(realFolder);
 
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AuthScreen()));
-                    // Either the permission was already granted before or the user just granted it.
-                  }
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  color: kblue,
-                  height: 40,
-                  width: MediaQuery.of(context).size.width,
-                  child: Text(
-                    'GRANT ACCESS',
-                    style: TextStyle(
-                        fontSize: 17,
-                        color: kwhite,
-                        fontWeight: FontWeight.w500),
-                  ),
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AuthScreen()));
+                  // Either the permission was already granted before or the user just granted it.
+                }
+              },
+              child: Container(
+                alignment: Alignment.center,
+                color: kblue,
+                height: 40,
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  'GRANT ACCESS',
+                  style: TextStyle(
+                      fontSize: 17, color: kwhite, fontWeight: FontWeight.bold),
                 ),
               ),
-              const SizedBox(height: 25),
-              TextButton(
-                child: const Text('EXIT'),
-                onPressed: () => exit(0),
-              )
-            ]),
-          ),
+            ),
+            const SizedBox(height: 25),
+            TextButton(
+              child: const Text(
+                'EXIT',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () => exit(0),
+            )
+          ]),
         ),
       ),
     );
