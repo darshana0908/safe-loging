0c5c8c686c14e08f6dabd33dfc1bed0d088ca1da
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-09-30 15:37:13 +0530

    file picker testing

---
+++ lib/services/file_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FilePage extends StatefulWidget {
+  const FilePage({Key? key}) : super(key: key);
+
+  @override
+  State<FilePage> createState() => _FilePageState();
+}
+
+class _FilePageState extends State<FilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
