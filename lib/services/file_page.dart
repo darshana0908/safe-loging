c862f65c2e7c8dd5dbdac1df686c4cf32ed92e46
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-10-03 11:30:06 +0530

    testing

--- lib/services/file_page.dart
+++ lib/services/file_page.dart
@@ -1,9 +1,11 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class FilePage extends StatefulWidget {
-  const FilePage({Key? key}) : super(key: key);
+  final List<PlatformFile> files;
+  final ValueChanged<PlatformFile> onOpendFile;
+  const FilePage({Key? key, required this.files, required this.onOpendFile})
+      : super(key: key);
 
   @override
   State<FilePage> createState() => _FilePageState();
@@ -11,7 +13,63 @@ class FilePage extends StatefulWidget {
 
 class _FilePageState extends State<FilePage> {
   @override
+  void initState() {
+    print(widget.onOpendFile);
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            crossAxisSpacing: 2, mainAxisSpacing: 8, maxCrossAxisExtent: 10),
+        itemCount: widget.files.length,
+        itemBuilder: (context, index) {
+          final file = widget.files[index];
+          return buildFile(file);
+        },
+      ),
+    );
+  }
+
+  Widget buildFile(PlatformFile file) {
+    final kb = file.size / 1024;
+    final mb = kb / 1024;
+    final fileSize =
+        mb >= 1 ? '${mb.toStringAsFixed(2)}MB' : '${kb.toStringAsFixed(2)}KB';
+    final extension = file.extension ?? 'none';
+    const color = Colors.amberAccent;
+    return InkWell(
+      onTap: () => widget.onOpendFile(file),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  '.$extension',
+                  style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
