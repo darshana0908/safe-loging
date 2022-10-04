2ab7ce2e4363a232a10f0a35ee7b36942f628a55
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-10-04 15:46:42 +0530

    un used file deleted

--- lib/screens/features/settings/settings.dart
+++ lib/screens/features/settings/settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:safe_encrypt/constants/colors.dart';
 
+import '../../../services/icon.dart';
+
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
@@ -19,151 +21,176 @@ class _SettingsState extends State<Settings> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             const SizedBox(
               height: 20,
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                
                 Card(
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Advanced',
-                        style: TextStyle(color: kliteblue, fontSize: 17),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Icon(Icons.warning_outlined),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: Text(
-                              'Break-In alerts',
-                              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const Divider(),
-                    const SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.alt_route_rounded,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Advanced',
+                            style: TextStyle(color: kliteblue, fontSize: 17),
                           ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: Text(
-                              'Fake PIN',
-                              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                            ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.warning_outlined),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 50),
+                                child: Text(
+                                  'Break-In alerts',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              )
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                    const Divider(),
-                    const SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.vpn_key,
+                        ),
+                        const Divider(),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: const [
+                              Icon(
+                                Icons.alt_route_rounded,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 50),
+                                child: Text(
+                                  'Fake PIN',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ],
                           ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: Text(
-                              'Sectret Door',
-                              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const Divider(),
-                    const SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Icon(Icons.lock),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: Text(
-                              'Album Lock',
-                              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ]),
+                        ),
+                        const Divider(),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.vpn_key,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => const AppIcon(),
+                                      ));
+                                },
+                                child: const Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 50),
+                                  child: Text(
+                                    'Secret Door',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Divider(),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.lock),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 50),
+                                child: Text(
+                                  'Album Lock',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ]),
                 ),
                 Card(
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Info',
-                        style: TextStyle(color: kliteblue, fontSize: 17),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.help,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Info',
+                            style: TextStyle(color: kliteblue, fontSize: 17),
                           ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: Text(
-                              'Help & support',
-                              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    const Divider(),
-                    const SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: const [
-                          Icon(Icons.info),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 50),
-                            child: Text(
-                              'Abouth',
-                              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                            ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: const [
+                              Icon(
+                                Icons.help,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 50),
+                                child: Text(
+                                  'Help & support',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              )
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                  ]),
+                        ),
+                        const Divider(),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.info),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 50),
+                                child: Text(
+                                  'Abouth',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]),
                 ),
               ],
             ),
