d1d25c03fc82734acdb393a52830949003bd3e58
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-10-07 15:01:11 +0530

    sharedpreffrence

--- lib/providers/google_sign_provider.dart
+++ lib/providers/google_sign_provider.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GoogleSignInProvider extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
@@ -24,5 +25,37 @@ class GoogleSignInProvider extends ChangeNotifier {
     await FirebaseAuth.instance.signInWithCredential(credential);
 
     notifyListeners();
+  }
+
+  String profileImage = '';
+  String get getProfileImageFront => profileImage;
+
+  void setProfileFront(String val) {
+    profileImage = val;
+    notifyListeners();
+  }
+
+  Future<String> getProfileImage(String pin) async {
+    // Obtain shared preferences.
+    final prefs = await SharedPreferences.getInstance();
+    String? newPath = prefs.getString(pin);
+    // print("------------------------------------");
+    // print(pin);
+    // print("------------------------------------");
+
+
+    setProfileFront(newPath!);
+
+    return newPath;
+  }
+
+  Future<void> setProfileImage(String pin, String imagePath) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString(pin, imagePath);
+   print("------------------------------------"); print(pin);
+   print(imagePath);
+    print("------------------------------------");
+    setProfileFront(imagePath);
   }
 }
