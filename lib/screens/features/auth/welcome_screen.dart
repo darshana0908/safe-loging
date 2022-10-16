f3c2da3d91425688fb46864c69674b4109c24a95
darshana0908 <46338262+darshana0908@users.noreply.github.com>
2022-10-17 04:54:53 +0530

    wrap widget

--- lib/screens/features/auth/welcome_screen.dart
+++ lib/screens/features/auth/welcome_screen.dart
@@ -31,59 +31,64 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kdarkblue,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-                icon: Icon(
-                  Icons.arrow_back,
-                  color: kwhite,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-            Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Column(children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-                Text(
-                  'Welcome back!',
-                  style: TextStyle(fontSize: 25, color: kwhite, fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text('Log in with your Keepsafe account email',
-                    style: TextStyle(fontSize: 17, color: Colors.white60, fontWeight: FontWeight.w500)),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(getemail.toString(), style: TextStyle(color: kwhite)),
-                const SizedBox(
-                  height: 40,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const FirstPinNumber()),
-                    );
-                  },
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: kblue,
-                    height: 40,
-                    width: MediaQuery.of(context).size.width,
-                    child: Text(
-                      'NEXT',
-                      style: TextStyle(fontSize: 17, color: kwhite, fontWeight: FontWeight.w500),
-                    ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: kwhite,
                   ),
-                )
-              ]),
-            ),
-          ],
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Column(children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+                  Text(
+                    'Welcome back!',
+                    style: TextStyle(fontSize: 25, color: kwhite, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text('Log in with your Keepsafe account email',
+                      style: TextStyle(fontSize: 21, color: Colors.white60, fontWeight: FontWeight.w500)),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    getemail.toString(),
+                    style: TextStyle(color: kwhite, fontSize: 19),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const FirstPinNumber()),
+                      );
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: kblue,
+                      height: 40,
+                      width: MediaQuery.of(context).size.width,
+                      child: Text(
+                        'NEXT',
+                        style: TextStyle(fontSize: 17, color: kwhite, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
